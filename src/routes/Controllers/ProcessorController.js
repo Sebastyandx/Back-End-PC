6e1cc68e1e929dc06db43c38297678055730eaf3
Tobi Tortosa <tobiastortosa17@gmail.com>
2022-10-26 20:23:35 -0300

    added new routes

--- src/routes/Controllers/ProcessorController.js
+++ src/routes/Controllers/ProcessorController.js
@@ -1,19 +1,42 @@
-const {Procesador} = require('../../db');
+const { Procesador } = require("../../db");
 
 module.exports = {
-    getDBproces: async (id) =>{
-        try{
-          if(id){
-            const oneProcess = await Procesador.findAll({
-              where: {id:id}
-            })
-            return oneProcess
-          }else{
-            const allProcess = await Procesador.findAll()
-            return allProcess
-          }
-        }catch(err){
-          return err
-        }
+  getAllproces: async (id) => {
+    try {
+      if (id) {
+        const oneProcess = await Procesador.findAll({
+          where: { id: id },
+        });
+        return oneProcess;
+      } else {
+        const allProcess = await Procesador.findAll();
+        return allProcess;
       }
-}
+    } catch (err) {
+      return err;
+    }
+  },
+
+  createProces: async (name, brand, img, details, cost) => {
+    const nuevoProcesador = await Procesador.create({
+      name,
+      brand,
+      details,
+      img,
+      cost,
+    });
+    return nuevoProcesador;
+  },
+
+  deleteProces: async (id) => {
+    await Procesador.destroy({ where: { id: id } });
+    return `Procesador con id ${id} borrado con exito`;
+  },
+
+  changeProces: async (name, brand, img, details, cost, id) => {
+    // const proces = await Procesador.findByPk(id);
+    // console.log(proces, "aaa");
+    await Procesador.update({ name, brand, cost }, { where: { id } });
+    return "s";
+  },
+};
--- src/routes/getAllProducts.js
+++ src/routes/getAllProducts.js
@@ -1,13 +1,18 @@
-const {Router} = require('express')
-const {getAllProducts} = require('./Controllers/GetAllProductsController')
-const router = Router()
+const { Router } = require("express");
+const { getAllProducts } = require("./Controllers/GetAllProductsController");
+const router = Router();
 
-router.get('', async (req, res) => {
-    try {
-        res.status(200).json(await getAllProducts())
-    } catch (error) {
-        res.send(error)
+router.get("/", async (req, res) => {
+  const { name } = req.body;
+  try {
+    if (name) {
+      res.status(200).json(await getAllProducts(name));
+    } else {
+      res.status(200).json(await getAllProducts());
     }
-})
+  } catch (error) {
+    res.send(error);
+  }
+});
 
-module.exports = router
+module.exports = router;
