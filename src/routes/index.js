a9ed57103418881c8051f1439c3410c1f954c5e2
gonzalogaete <gonzalogaete110@gmail.com>
2022-10-26 13:41:49 -0300

    ruta de procesador lista

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,13 @@
 const { Router } = require("express");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-const pokemonRoute = require("./pokemons");
-const typeRoute = require("./type");
-
+// const pokemonRoute = require("./pokemons");
+const RuterProcesador = require("../routes/Controllers/ProcesadoresController");
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-router.use("/mothers", pokemonRoute);
-router.use("/procesador", typeRoute);
+// router.use("/mothers", pokemonRoute);
+router.use("/procesadores", RuterProcesador);
 
 module.exports = router;
