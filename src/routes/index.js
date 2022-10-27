6f7f6feb07c3943fa38b4bd5f9479baeda862a94
sebastian <sebastyandx@hotmail.com>
2022-10-27 17:39:44 -0300

    los JSON con types

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,13 @@
 const { Router } = require("express");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-const pokemonRoute = require("./pokemons");
-const typeRoute = require("./type");
-const motherBoard = require('./motherBoard.js');
+const motherBoard = require("./motherBoard.js");
 const RuterProcesador = require("../routes/Controllers/ProcesadoresController");
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-router.use("/motherBoard", motherBoard
+router.use("/motherBoard", motherBoard);
 router.use("/procesadores", RuterProcesador);
 
 module.exports = router;
