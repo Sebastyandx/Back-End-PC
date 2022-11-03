68c1177ff3548ba322bad3a50a13c0647603e875
fabio mansilla <fabio.mansilla07@gmail.com>
2022-11-03 16:35:51 -0300

    lastchange

--- src/routes/products.js
+++ src/routes/products.js
@@ -44,15 +44,15 @@ router.get('/:id', async (req,res) => {
     }
 })
 
-router.post("/create",userExtractor , async(req, res) => {
+router.post("/create", async(req, res) => {
 
-    const {name, brand, cost, type} = req.body;
+    const {name, brand, cost, type, img, details} = req.body;
     console.log(req.body)
     try {
         if(!name || !brand || !cost || !type ) {
             return res.status(404).send("no se enviaron los requerimientos necesesarios")
         }
-        const productCreate = await Producto.create({name, brand, cost, type});
+        const productCreate = await Producto.create({name, brand, cost, type, img, details});
         res.json(productCreate);
     } catch (error) {
         res.send(error)
