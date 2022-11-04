a3380a32832c209d7be3d25395bb5ebcf78329fd
sebastian <sebastyandx@hotmail.com>
2022-11-03 21:34:06 -0300

    Stripe andando, falta acomodar la URL

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,17 +2,19 @@ const { Router } = require("express");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
-const allProducts = require('./products.js');
-const users = require('./users')
-const usersLogin = require('./login')
-const cart = require('./cart')
+const allProducts = require("./products.js");
+const users = require("./users");
+const usersLogin = require("./login");
+const cart = require("./cart");
+const stripe = require("./pasarela");
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
 router.use("/productos", allProducts);
-router.use("/user/create", users)
-router.use("/user/login", usersLogin)
-router.use('/cart', cart)
+router.use("/user/create", users);
+router.use("/user/login", usersLogin);
+router.use("/cart", cart);
+router.use("/create-checkout-session", stripe);
 module.exports = router;
