7b1179a3c403607a4c85508db76f32a17b1a09e9
Sebastian Rodriguez <67121520+Sebastyandx@users.noreply.github.com>
2023-01-04 22:45:44 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -27,7 +27,7 @@ server.use((req, res, next) => {
 
 server.use("/", routes);
 
-// Error catching endware.
+// Error catching endware
 server.use((err, req, res, next) => {
   // eslint-disable-line no-unused-vars
   const status = err.status || 500;
